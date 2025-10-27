f48893dec0f982ef126acde85ec0ad1d15ca1b6b
M <fourmayaa@gmail.com>
2025-10-27 22:26:23 +0530

    oct27

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+export const Home = () => {
+  return (
+    <div>
+      {/* theme toggle */}
+
+      {/* bg effects */}
+
+      {/* navbar */}
+
+      {/* main content */}
+
+      {/* footer*/}
+    </div>
+  );
+};
