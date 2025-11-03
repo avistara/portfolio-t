b4762754c9cfd85d4bda1c5e3fd6c3803070b2aa
M <fourmayaa@gmail.com>
2025-11-03 20:32:01 +0530

    stars+meteors, 1hour

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* theme toggle */}
       <ThemeToggle />
       {/* bg effects */}
-
+      <StarBackground />
       {/* navbar */}
 
       {/* main content */}
